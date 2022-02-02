f331fd60e71fe4058282723c4d1434c307395e5e
Gerardo <39634874+geramrico@users.noreply.github.com>
2022-02-02 16:15:52 +0000

    login function

--- index.js
+++ index.js
@@ -8,4 +8,19 @@ const hashPassword = async (pw) =>{
   console.log(hash)
 }
 
+
+//Compares the password with its hash
+const login = async(pw,hashedPw) => {
+  const result = await bcrypt.compare(pw,hashedPw)
+  if(result){
+    console.log('You are in!')
+  } else{
+    console.log('WRONG')
+  }
+}
+
+
+
 hashPassword('hi')
+
+login('hi!','$2b$10$r4ElZWd7GRfVAJb7lNer/uuQFj8HwZCKnilSs9lb9fKFQHRF5sXvS')
